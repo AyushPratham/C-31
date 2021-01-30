1a33dff9d5be9bcfe60b8d2fcf8ae79848210a53
AyushPratham <ayushpratham10@gmail.com>
2021-01-29 19:59:44 -0700

    SmokeAdded

--- sketch.js
+++ sketch.js
@@ -7,13 +7,14 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gamestate = "onSling"
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
 
 function setup(){
+
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
@@ -69,12 +70,15 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gamestate === "onSling"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+}
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gamestate = "launched"
 }
 
 function keyPressed(){
